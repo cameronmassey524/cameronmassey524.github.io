81f7d54aff84bd782b78fb01391c4753d69de790
cameronmassey524 <78172804+cameronmassey524@users.noreply.github.com>
2021-04-02 19:14:16 -0400

    Implemented movable board pieces.
    Board pawns are now selectable, movable, and attack, and die.

--- cs325-game-prototypes/digital-assignment-6/js/main.js
+++ cs325-game-prototypes/digital-assignment-6/js/main.js
@@ -15,15 +15,162 @@ var board_size;
 var p1_order;
 var p2_order;
 var turn;
+var board;
+var selection = 0;
+var cursorKeys;
+var hud;
 
 //class Pawn {
 //}
 
+function factorial(n)
+{
+	var product = 1;
+	for (var i=2; i<=n; ++i)
+	{
+		product*=i;
+	}
+	return product;
+}
+
+class Hud {
+	constructor(scene)
+	{
+		this.turn = 1;
+		this.p1_name = "Player1"
+		this.p1_energy = board_size;
+		this.p1_health = factorial(board_size);
+		this.p2_name = "Player2";
+		this.p2_energy = 0;
+		this.p2_health = factorial(board_size);
+		this.p1_name_label = scene.add.text(10, 10, this.p1_name, { font: '20px Courier', fill: '#ffffff' });
+		this.p1_energy_label = scene.add.text(10, 300, ("Energy: \n" + this.p1_energy), { font: '20px Courier', fill: '#ffffff' });
+		this.p1_health_label = scene.add.text(10, 500, ("Health: \n" + this.p1_health), { font: '20px Courier', fill: '#ffffff' });
+		this.p2_name_label = scene.add.text(710, 10, this.p2_name, { font: '20px Courier', fill: '#ffffff' });
+		this.p2_energy_label = scene.add.text(710, 300, ("Energy: \n" + this.p2_energy), { font: '20px Courier', fill: '#ffffff' });
+		this.p2_health_label = scene.add.text(710, 500, ("Health: \n" + this.p2_health), { font: '20px Courier', fill: '#ffffff' });
+	}
+	
+	update()
+	{
+	}
+	
+	set_p1_energy(x)
+	{
+		p1_energy = x;
+		p1_energy_label.setText(("Energy: " + x));
+	}
+
+	set_p1_health(x)
+	{
+		p1_health = x;
+		p1_health_label.setText(("Health: " + x));
+	}
+
+	set_p2_energy(x)
+	{
+		p2_energy = x;
+		p2_energy_label.setText(("Energy: " + x));
+	}
+
+	set_p2_health(x)
+	{
+		p2_health = x;
+		p2_health_label.setText(("Health: " + x));
+	}
+	
+}
+
 class Tile {
-  constructor(pawn, owner, color) {
-	  this.pawn = 0;
+  constructor(col, row, strength, owner, selected) {
+	  this.strength = 0;
 	  this.owner = 0;
+	  this.col = col;
+	  this.row = row;
+	  this.circ = 0;
+	  this.text = 0;
+	  this.selected = 0;
   }
+  
+  deselect()
+  {
+	  this.selected = 0;
+  }
+  select()
+  {
+	  this.selected = 1;
+  }
+  swap(target)
+  {
+	  //[[this.col],[target.col]] =[[target.col],[this.col]];
+	  //[[this.row],[target.row]] =[[target.row],[this.row]];
+	  [[this.strength],[target.strength]] =[[target.strength],[this.strength]];
+	  [[this.owner],[target.owner]] =[[target.owner],[this.owner]];
+	  [[this.selected],[target.selected]] =[[target.selected],[this.selected]];
+	  //[[this], [target]] = [[target], [this]];
+	  return target;
+
+  }
+  attack(target)
+  {
+	  [[this.strength],[target.strength]] = [[this.strength-target.strength],[target.strength-this.strength]];
+	  
+	  if (this.strength <= 0)
+	  {
+		  this.owner = 0;
+		  this.selected = 0;
+	  }
+	  if (target.strength <= 0)
+	  {
+		  target.owner = 0;
+		  //target.selected = 0;
+	  }
+
+  }
+  
+  act(dir)
+  {
+	
+	var t_col = selection.col; //target column starting point
+	var t_row = selection.row; //taget row starting point
+	
+	switch(dir) // add or subtract 1 to t_col or t_row depending on direction
+	{
+		case 'r':
+			t_col += 1;
+			break;
+		case 'l':
+			t_col -= 1;
+			break;
+		case 'u':
+			t_row -= 1;
+			break;
+		case 'd':
+			t_row += 1;
+			break;
+	}
+	
+	var target = board.mat[t_row][t_col];
+	if (target.owner==0) //Will move
+	{
+		console.log(dir + " Move\n");
+		selection = selection.swap(target);
+	}
+	else if (target.owner==selection.owner) //Teammate at destination
+	{
+		console.log("Blocked by teammate\n");
+	}
+	else if ( (t_row >= board_size) || (t_col >= board_size) || (t_row<0) || (t_col<0)) //No more board space
+	{
+		console.log("Blocked by Edge\n");
+	}
+	else //Attacking opponent's piece
+	{
+		console.log("Attacking opponent\n");
+		selection.attack(target);
+	}
+  }
+  
 }
 class Board {
 	constructor(size, p1, p2) {
@@ -50,7 +197,7 @@ class Board {
 		{
 			for (var col=0; col<size; ++col)
 			{
-				this.mat[col][row] = new Tile(0,0,0);
+				this.mat[row][col] = new Tile(col, row, 0, 0, 0);
 /* 				if (color_parity > 0) { mat[i][j].color = "0xbababa"; }
 				else { mat[i][j].color = "0x5c5c5c"; }
 				
@@ -64,17 +211,22 @@ class Board {
 		
 		for (var n=0; n<size; ++n)
 		{
-			this.mat[0][n].pawn = p1_order[n];
-			this.mat[0][n].owner = 1;
-			this.mat[size-1][n].pawn = p2_order[n];
-			this.mat[size-1][n].owner = 2;
+			//this.mat[0][n].strength = p1_order[n];
+			//this.mat[0][n].owner = 1;
+			//this.mat[size-1][n].strength = p2_order[n];
+			//this.mat[size-1][n].owner = 2;
+			
+			this.mat[n][0].strength = p1_order[n];
+			this.mat[n][0].owner = 1;
+			this.mat[n][size-1].strength = p2_order[n];
+			this.mat[n][size-1].owner = 2;
 		}
 	
 	}
 	
 	display(scene)
 	{
-		var grid = scene.add.grid(400, 300, 600, 600, (600/this.size), (600/this.size), 0xbababa).setAltFillStyle(0x5c5c5c).setOutlineStyle();
+		scene.add.grid(400, 300, 600, 600, (600/this.size), (600/this.size), 0xbababa).setAltFillStyle(0x5c5c5c).setOutlineStyle();
 		
 		var x_offset =  100 + (300/this.size);
 		var y_offset = (300/this.size);
@@ -89,25 +241,95 @@ class Board {
 				x_pos = x_offset + col*diff;
 				y_pos = y_offset + row*diff;
 				
-				if (this.mat[col][row].owner == 1)
+				if (this.mat[row][col].owner == 1)
 				{
-					scene.add.circle(x_pos, y_pos, (200/this.size), 0xb80000);
-					scene.add.text(x_pos-8, y_pos-16, this.mat[col][row].pawn, { font: '32px Courier', fill: '#ffffff' });
+					this.mat[row][col].circ = scene.add.circle(x_pos, y_pos, (200/this.size), 0xb80000);
+					if (this.mat[row][col].selected == 1)
+					{
+						this.mat[row][col].circ.setFillStyle(0x00b800);
+					}
+					this.mat[row][col].text = scene.add.text(x_pos-8, y_pos-16, this.mat[row][col].strength, { font: '32px Courier', fill: '#ffffff' });
 				}
-				else if (this.mat[col][row].owner == 2)
+				else if (this.mat[row][col].owner == 2)
 				{
-					scene.add.circle(x_pos, y_pos, (200/this.size), 0x0000b8);
-					scene.add.text(x_pos-8, y_pos-16, this.mat[col][row].pawn, { font: '32px Courier', fill: '#ffffff' });
+					this.mat[row][col].circ = scene.add.circle(x_pos, y_pos, (200/this.size), 0x0000b8);
+					if (this.mat[row][col].selected == 1)
+					{
+						this.mat[row][col].circ.setFillStyle(0x00b800);
+					}
+					this.mat[row][col].text = scene.add.text(x_pos-8, y_pos-16, this.mat[row][col].strength, { font: '32px Courier', fill: '#ffffff' });
 				}
+				
 				
 				
 			}
 		}
 	}
-
-
-
 	
+	click_to_tile(x, y)
+	{
+		if ((x<100) || (x>700))
+		{
+			return;
+		}
+		
+		var row = Math.floor(y/(600/board_size));
+		var col = Math.floor((x-100)/(600/board_size));
+		//console.log("calculated col: %d\n", col);
+		//console.log("calculated row: %d\n", row);
+		return this.mat[row][col];
+	}
+	
+	
+	select_pawn(target)
+	{
+		if (target.owner == 0)
+		{
+			return;
+		}
+		target.selected = 1;
+	}
+	
+	get_tile(c, r)
+	{
+		return this.mat[r][c];
+	}
+	
+	swap_tiles(c1, r1, c2, r2)
+	{
+		var temp = this.mat[c1][r1];
+		this.mat[c1][r1] = this.mat[c2][r2];
+		this.mat[c2][r2] = temp;
+	}
+	
+	swap(t1, t2)
+	{
+		var temp = t1;
+		t1 = t2;
+		t2 = temp;
+	}
+	
+	move_pawn(pawn, dir)
+	{
+		if (pawn.selected!=1)
+		{
+			return;
+		}
+		if (dir=="u")
+		{
+			if (pawn.row-1>=0)
+			{
+				this.swap_tiles(pawn.c, pawn.r, pawn.c, pawn.r-1);
+			}
+		}
+		if (dir=="r")
+		{
+			if (pawn.col+1<board_size)
+			{
+				this.swap_tiles(pawn.c, pawn.r, pawn.c+1, pawn.r);
+			}
+		}
+	}
 	
 
 }
@@ -260,6 +482,8 @@ var SceneC = new Phaser.Class({
 		}
 		
 	})
+	
+
         
     }
 
@@ -284,10 +508,89 @@ var SceneD = new Phaser.Class({
     create: function ()
     {
 		
-		var board = new Board(board_size, p1_order, p2_order);
+		cursorKeys = this.input.keyboard.createCursorKeys();
+		var key_up = this.input.keyboard.addKey('W');
+		var key_down = this.input.keyboard.addKey('S');
+		var key_left = this.input.keyboard.addKey('A');
+		var key_right = this.input.keyboard.addKey('D');
+		var temp;
+		var target;
+
+		
+		var grid;
+		board = new Board(board_size, p1_order, p2_order);
+		//this.add.grid(400, 300, 600, 600, (600/this.size), (600/this.size), 0xbababa).setAltFillStyle(0x5c5c5c).setOutlineStyle();
 		board.display(this);
+		
+		hud = new Hud(this);
+		
+		this.input.on('pointerdown', function (pointer) {
+
+			if ((selection.selected==1)) //deselect current (old) selection
+			{
+				//console.log("selection == %d\n", selection);
+				//console.log("selection.selected == %d\n", selection.selected);
+				selection.deselect();
+			}
+			
+			//console.log('down');
+			//console.log("x: %d\ny: %d", pointer.x, pointer.y);
+			selection = board.click_to_tile(pointer.x, pointer.y);
+			console.log("Owner: %d\n", selection.owner);
+			console.log("Strength: " + selection.strength + "\n");
+			if (selection.owner == 0)
+			{
+				board.display(this);
+				return;
+			}
+			board.select_pawn(selection);
+			board.display(this);
+			
+
+		}, this);
+		
+		key_right.on('down', function(event) {
+			if ((selection.owner ==1)||(selection.owner==2)) 
+			{
+				selection.act('r');
+				board.display(this);
+			}
+			
+		}, this );
+		key_left.on('down', function(event) {
+			if ((selection.owner ==1)||(selection.owner==2)) 
+			{
+				selection.act('l');
+				board.display(this);
+			}
+			
+		}, this );
+		key_up.on('down', function(event) {
+			if ((selection.owner ==1)||(selection.owner==2)) 
+			{
+				selection.act('u');
+				board.display(this);
+			}
+			
+		}, this );
+		key_down.on('down', function(event) {
+			if ((selection.owner ==1)||(selection.owner==2)) 
+			{
+				selection.act('d');
+				board.display(this);
+			}
+			
+		}, this );
+		
+		
+		
         
-    }
+    },
+	
+	update: function ()
+	{
+
+	}
 
 });
 
